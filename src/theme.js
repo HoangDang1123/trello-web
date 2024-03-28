66efcd563db67edd22d82aca5103f27a7e6ca085
HoangDang1123 <110187412+HoangDang1123@users.noreply.github.com>
2024-03-28 23:45:15 +0700

    Refactor Project Structure

--- src/theme.js
+++ src/theme.js
@@ -3,18 +3,25 @@ import { experimental_extendTheme as extendTheme} from '@mui/material/styles'
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '52px',
+    boardBarHeight: '58px'
+  },
+
   colorSchemes: {
     light: {
       palette: {
         primary: {main: '#4169E1'},
         secondary: {main: '#FF5733'},
-      }
+      },
+      //spacing: (factor) => `${0.25 * factor}rem` // (Bootstrap strategy)
     },
     dark: {
       palette: {
         primary: {main: '#6495ED'},
         secondary: {main: '#FFA500'},
-      }
+      },
+      //spacing: (factor) => `${0.25 * factor}rem` // (Bootstrap strategy)
     }
   }
   // ...other properties
